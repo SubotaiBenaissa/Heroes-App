f95c1b2a629a1e4062c503fc5a43186e66197003
Alangobe <alangobe01@gmail.com>
2022-09-05 13:22:12 -0500

    Hecho navbar y archivos de barril

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,12 +3,14 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { LoginPage } from '../auth/pages/LoginPage'
 import { DcPage } from '../heroes/pages/DcPage'
 import { MarvelPage } from '../heroes/pages/MarvelPage'
+import { NavbarComponent } from '../ui'
 
 export const AppRouter = () => {
 
     return (
         
         <>
+            <NavbarComponent />
             <Routes>
                 <Route path='marvel' element={ <MarvelPage /> }/>
                 <Route path='dc' element={ <DcPage /> } />
