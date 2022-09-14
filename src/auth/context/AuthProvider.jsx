01356eeef9a9c33c9d6c0a20d5638e34c4593ac2
Alangobe <alangobe01@gmail.com>
2022-09-14 18:32:07 -0500

    Guardado en localstorage el user

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,23 +4,31 @@ import { AuthContext } from './AuthContext'
 import { authReducer } from './authReducer'
 import { types } from '../types/types'
 
-const initialState = {
-    logged: false
+const init = () => {
+
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user: user
+    }
+
 }
 
 export const AuthProvider = ({ children }) => {
 
-    const [ state, dispatch ] = useReducer( authReducer, initialState );
+    const [ state, dispatch ] = useReducer( authReducer, {}, init );
 
     const login = ( name = '' ) => {
 
+        const user = {id: 'ABC', name}
+
         const action = {
             type: types.login,
-            payload: {
-                id: 'ABC',
-                name: name
-            }
+            payload: user
         }
+
+        localStorage.setItem('user', JSON.stringify( user ))
 
         dispatch(action);
 
