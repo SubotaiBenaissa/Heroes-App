da2e41d08e49e403062a7c51fb2c5fb1dbbb303d
Alangobe <alangobe01@gmail.com>
2022-09-15 19:59:50 -0500

    Añadidas rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { LoginPage } from '../auth/pages/LoginPage'
 import { HeroesRoutes } from '../heroes'
+import { PrivateRoute } from './PrivateRoute'
 
 export const AppRouter = () => {
 
@@ -10,7 +11,12 @@ export const AppRouter = () => {
         <>
             <Routes>
                 <Route path='login' element={ <LoginPage /> } />
-                <Route path='/*' element={ <HeroesRoutes /> } />
+                <Route path='/*' 
+                element={ 
+                <PrivateRoute>
+                    <Route path='/*' element={ <HeroesRoutes /> } />
+                </PrivateRoute> 
+                } />            
             </Routes>
         </>
 
